a4824cce0e6808176d52d38cb11992a29394416b
moisebrice <moisebrice@yahoo.fr>
2015-11-17 13:29:12 +0100

    nn

--- DITACOURRIER_MINATD/src/cm/dita/service/domaine/impl/EspaceRessourceServiceImpl.java
+++ DITACOURRIER_MINATD/src/cm/dita/service/domaine/impl/EspaceRessourceServiceImpl.java
@@ -2,6 +2,7 @@ package cm.dita.service.domaine.impl;
 
 import java.io.Serializable;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Map;
 
 import cm.dita.service.generic.ServiceBaseImpl;
@@ -10,6 +11,7 @@ import cm.dita.dao.domaine.inter.IEspaceCourrierDao;
 import cm.dita.dao.domaine.inter.IEspaceDao;
 import cm.dita.dao.generic.IDaoBase;
 import cm.dita.entities.Espace;
+import cm.dita.entities.EspaceCourrier;
 import cm.dita.entities.user.Group;
 import cm.dita.service.domaine.inter.IEspaceRessourceService;
 
@@ -39,6 +41,37 @@ public class EspaceRessourceServiceImpl  extends ServiceBaseImpl<Espace>  implem
 	    			return true;
 	    	
 	    }
+	public List<Espace> getMyEspace(Espace espace){
+		String chaine="%"+espace.getHierachie()+"*"+espace.getId();
+		//JOptionPane.showMessageDialog(null, chaine+" "+espace.getId());
+		@SuppressWarnings("unchecked")
+		List<Espace> liste = getDao().getCurrentSession().createQuery("select e from Espace e  where e.delate='false' "
+				+ " AND e.hierachie LIKE :chaine AND e.id<>:id").setParameter("chaine", chaine).setParameter("id", espace.getId()).getResultList();
+   		
+		return liste;
+	}
+	
+	public void updateHierachie(String new_hie,Espace espace){
+		String last_hie=espace.getHierachie()+"*"+espace.getId();
+		int taille=-1;
+		//new_hie=new_hie+"*"+espace.getId();
+		
+		if(espace.getHierachie()!=null){
+			new_hie=new_hie+"*"+espace.getId();
+			taille=(espace.getHierachie()+"*"+espace.getId()).length()+1;
+			last_hie=espace.getHierachie()+"*"+espace.getId()+"%";
+			
+			
+			String query="UPDATE Espace e SET e.hierachie =CONCAT(:new,SUBSTRING(e.hierachie,:taille))"
+					+ " WHERE e.hierachie LIKE :chaine";//
+			getDao().getCurrentSession().createQuery(query).setParameter("new",new_hie).setParameter("taille",taille)
+			.setParameter("chaine", last_hie).executeUpdate();
+			
+			
+		}
+		
+		
+	}
 	
 	public IEspaceDao getDao() {
 		return dao;
@@ -48,6 +81,15 @@ public class EspaceRessourceServiceImpl  extends ServiceBaseImpl<Espace>  implem
 	 */
 	public void setDao(IEspaceDao dao) {
 		this.dao = dao;
+	}
+
+	@Override
+	public List<Espace> listeFonctionNonAttribue() {
+		// TODO Auto-generated method stub
+		
+		List<Espace> listeEspace = getDao().getCurrentSession().createQuery("select e from Espace e where e.used=false and e.delate='false'").getResultList();
+   		
+		return listeEspace;
 	}
 	
 
