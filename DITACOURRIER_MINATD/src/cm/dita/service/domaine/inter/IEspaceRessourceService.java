57592c8f784e87b684f9ec28465a2c9bbbea113b
moisebrice <moisebrice@yahoo.fr>
2015-11-17 13:35:23 +0100

    hh

--- DITACOURRIER_MINATD/src/cm/dita/service/domaine/inter/IEspaceRessourceService.java
+++ DITACOURRIER_MINATD/src/cm/dita/service/domaine/inter/IEspaceRessourceService.java
@@ -1,9 +1,16 @@
 package cm.dita.service.domaine.inter;
 
+import java.util.List;
+
 import cm.dita.service.generic.IServiceBase;
 import cm.dita.entities.Espace;
+import cm.dita.entities.user.User;
 
 public interface IEspaceRessourceService extends IServiceBase<Espace>{
 
 	public  boolean userExiste(Espace espace);
+	public List<Espace> getMyEspace(Espace espace);
+	
+	public void updateHierachie(String new_hie,Espace espace);
+	public List<Espace> listeFonctionNonAttribue();
 }
